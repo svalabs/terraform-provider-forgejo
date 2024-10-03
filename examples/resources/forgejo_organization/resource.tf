1d6651a40b075b1a7c8061b68ef5cb5b6307051c
Achim Christ <acch@users.noreply.github.com>
2024-10-03 17:27:02 +0000

    feat: enhanced usage examples

--- examples/resources/forgejo_organization/resource.tf
+++ examples/resources/forgejo_organization/resource.tf
@@ -10,12 +10,18 @@ provider "forgejo" {
   host = "http://localhost:3000"
 }
 
-resource "forgejo_organization" "this" {
-  name        = "tftest"
-  full_name   = "Terraform Test Org"
+resource "forgejo_organization" "defaults" {
+  name = "tftest_defaults"
+}
+resource "forgejo_organization" "non_defaults" {
+  name        = "tftest_non_defaults"
+  full_name   = "Terraform Test Org with non-default attributes"
   description = "Purely for testing..."
+  website     = "https://forgejo.org/"
+  location    = "Mêlée Island"
+  visibility  = "private"
 }
 
 output "debug" {
-  value = forgejo_organization.this
+  value = forgejo_organization.defaults
 }
