802d6e98a8342b20e9b8586ff3ba13354ab564e6
Achim Christ <acch@users.noreply.github.com>
2024-12-17 14:39:22 +0000

    fix: distinguish between user & org repo

--- examples/data-sources/forgejo_repository/data-source.tf
+++ examples/data-sources/forgejo_repository/data-source.tf
@@ -10,13 +10,24 @@ provider "forgejo" {
   host = "http://localhost:3000"
 }
 
-data "forgejo_repository" "this" {
+data "forgejo_repository" "user" {
   owner = {
     login = "achim"
   }
-  name = "test1"
+  name = "user_test_repo_1"
 }
 
-output "debug" {
-  value = data.forgejo_repository.this
+data "forgejo_repository" "org" {
+  owner = {
+    login = "test_org_1"
+  }
+  name = "org_test_repo_1"
+}
+
+output "user_debug" {
+  value = data.forgejo_repository.user
+}
+
+output "org_debug" {
+  value = data.forgejo_repository.org
 }
