4fa61e431f6f3cff6b24b89c0696a1d7527bcbaa
Achim Christ <acch@users.noreply.github.com>
2025-02-08 19:00:26 +0000

    chore: rename repo for readability

--- examples/resources/forgejo_deploy_key/resource.tf
+++ examples/resources/forgejo_deploy_key/resource.tf
@@ -19,13 +19,13 @@ resource "tls_private_key" "ed25519" {
 }
 
 # Personal repository
-resource "forgejo_repository" "personal_defaults" {
-  name = "personal_test_repo_defaults"
+resource "forgejo_repository" "personal" {
+  name = "personal_test_repo"
 }
 
 # Deploy key
 resource "forgejo_deploy_key" "this" {
-  repository_id = forgejo_repository.personal_defaults.id
+  repository_id = forgejo_repository.personal.id
   key           = trimspace(tls_private_key.ed25519.public_key_openssh)
   title         = "test_key"
   read_only     = false
