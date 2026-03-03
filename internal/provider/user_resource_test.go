16875a5bdac770686e59479baa2d345892ba7d99
hollac1 <corneel.vanholland@kadaster.nl>
2026-03-03 07:30:45 +0100

    feat(user): Allow deactivating users on destroy.

--- internal/provider/user_resource_test.go
+++ internal/provider/user_resource_test.go
@@ -283,6 +283,129 @@ resource "forgejo_user" "test" {
 					statecheck.ExpectSensitiveValue("forgejo_user.test", tfjsonpath.New("password")),
 				},
 			},
+			// Deactivate on destroy testing -- creation/updating
+			{
+				Config: providerConfig + `
+resource "forgejo_user" "test" {
+	login                 = "tftest1"
+	email                 = "tftest1@localhost.localdomain"
+	password              = "passw1rd"
+	deactivate_on_destroy = true
+}`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("active"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("admin"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("allow_create_organization"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("allow_git_hook"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("allow_import_local"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("avatar_url"), knownvalue.StringRegexp(regexp.MustCompile("^http://localhost:3000/avatars/[0-9a-z]{32}$"))),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("created_at"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("description"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("email"), knownvalue.StringRegexp(regexp.MustCompile(`^[0-9a-z]+@[a-z]+\.[a-z]+$`))),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("followers_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("following_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("full_name"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("html_url"), knownvalue.StringExact("http://localhost:3000/tftest1")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("language"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("last_login"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("location"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("login_name"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("login"), knownvalue.StringExact("tftest1")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("max_repo_creation"), knownvalue.Int64Exact(-1)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("must_change_password"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("prohibit_login"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("restricted"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("send_notify"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("source_id"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("starred_repos_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("visibility"), knownvalue.StringExact("private")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("website"), knownvalue.StringExact("")),
+					statecheck.ExpectSensitiveValue("forgejo_user.test", tfjsonpath.New("password")),
+				},
+			},
+			// Deactivate on destroy testing -- destroying
+			{
+				Config: providerConfig,
+			},
+			// Deactivate on destroy testing -- reading after destroy
+			{
+				Config: providerConfig + `
+data "forgejo_user" "test" {
+	login = "tftest1"
+}`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("active"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("admin"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("avatar_url"), knownvalue.StringRegexp(regexp.MustCompile("^http://localhost:3000/avatars/[0-9a-z]{32}$"))),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("created_at"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("description"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("email"), knownvalue.StringRegexp(regexp.MustCompile(`^[0-9a-z]+@[a-z]+\.[a-z]+$`))),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("followers_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("following_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("full_name"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("html_url"), knownvalue.StringExact("http://localhost:3000/tftest1")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("language"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("last_login"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("location"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("login_name"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("login"), knownvalue.StringExact("tftest1")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("prohibit_login"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("restricted"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("source_id"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("starred_repos_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("visibility"), knownvalue.StringExact("private")),
+					statecheck.ExpectKnownValue("data.forgejo_user.test", tfjsonpath.New("website"), knownvalue.StringExact("")),
+				},
+			},
+			// Deactivate on destroy testing -- importing reactivates
+			{
+				Config: providerConfig + `
+resource "forgejo_user" "test" {
+	login                 = "tftest1"
+	email                 = "tftest1@localhost.localdomain"
+	password              = "passw1rd"
+}
+data "forgejo_user" "import_test" {
+	login = "tftest1"
+}
+import {
+	id = "tftest1"
+	to = forgejo_user.test
+}`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("active"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("admin"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("allow_create_organization"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("allow_git_hook"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("allow_import_local"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("avatar_url"), knownvalue.StringRegexp(regexp.MustCompile("^http://localhost:3000/avatars/[0-9a-z]{32}$"))),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("created_at"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("description"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("email"), knownvalue.StringRegexp(regexp.MustCompile(`^[0-9a-z]+@[a-z]+\.[a-z]+$`))),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("followers_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("following_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("full_name"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("html_url"), knownvalue.StringExact("http://localhost:3000/tftest1")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("language"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("last_login"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("location"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("login_name"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("login"), knownvalue.StringExact("tftest1")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("max_repo_creation"), knownvalue.Int64Exact(-1)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("must_change_password"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("prohibit_login"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("restricted"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("send_notify"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("source_id"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("starred_repos_count"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("visibility"), knownvalue.StringExact("private")),
+					statecheck.ExpectKnownValue("forgejo_user.test", tfjsonpath.New("website"), knownvalue.StringExact("")),
+					statecheck.ExpectSensitiveValue("forgejo_user.test", tfjsonpath.New("password")),
+				},
+			},
 			// Delete testing automatically occurs in TestCase
 		},
 	})
