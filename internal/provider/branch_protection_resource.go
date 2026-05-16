25fec3be334029098679f20b57876fa878b780ae
Martin Weissbach <martin.weissbach@sva.de>
2026-05-16 20:51:27 +0200

    feat(branch-protection): implement push protection validation
    Implement validation for attributes regarding the enablement of
    push protections of a branch.
    If not push protection is enabled, it is also invalid to enable an
    allowlist for that branch.
    
    Subsequently, we also check allowlist-related attributes to be empty or
    false, respectively, if the push allowlist is disabled.
    
    part of #59
    
    Signed-off-by: Martin Weissbach <martin.weissbach@sva.de>

--- internal/provider/branch_protection_resource.go
+++ internal/provider/branch_protection_resource.go
@@ -28,9 +28,10 @@ import (
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
-	_ resource.Resource                = &branchProtectionResource{}
-	_ resource.ResourceWithConfigure   = &branchProtectionResource{}
-	_ resource.ResourceWithImportState = &branchProtectionResource{}
+	_ resource.Resource                     = &branchProtectionResource{}
+	_ resource.ResourceWithConfigure        = &branchProtectionResource{}
+	_ resource.ResourceWithImportState      = &branchProtectionResource{}
+	_ resource.ResourceWithConfigValidators = &branchProtectionResource{}
 )
 
 // branchProtectionResource is the resource implementation.
@@ -750,6 +751,13 @@ func (r *branchProtectionResource) ImportState(ctx context.Context, req resource
 	// Save data into Terraform state
 	diags = response.State.Set(ctx, &data)
 	response.Diagnostics.Append(diags...)
+}
+
+// ConfigValidators returns a list of configuration validators for the resource.
+func (r *branchProtectionResource) ConfigValidators(_ context.Context) []resource.ConfigValidator {
+	return []resource.ConfigValidator{
+		branchProtectionResourcePushConfigValidator{},
+	}
 }
 
 // NewBranchProtectionResource is a helper function to simplify the provider implementation.
